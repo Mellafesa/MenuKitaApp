69b5ad998149d6cecf7dc6aa8e775e3c6b6a38c0
Mellafesa <mellafesarofida@gmail.com>
2022-03-21 13:03:24 +0700

    Initial commit

---
+++ app/src/main/java/com/example/firebaseku/Users.kt
@@ -0,0 +1,8 @@
+package com.example.firebaseku
+
+class Users (var id: String, var nama : String, var jumlah: String, var deskripsi: String) {
+
+    constructor() : this("", "", "", "") {
+
+    }
+}
